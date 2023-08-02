0bedfc029ce6b4990ab6a3bad508bad8fc908dbe
IJTINK7 <ijtink7@mail.ru>
2023-08-02 16:46:51 +0300

    Creating Home.tsx file

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import {DataOneType} from "../App";
+type HomePropsType = {
+	data1: DataOneType
+}
+
+export const Home = (props: HomePropsType)=>{
+	return(
+		<div>
+			<h1>Home</h1>
+			<div>{props.data1.image}</div>
+			<button>{props.data1.button}</button>
+		</div>
+	);
+}
