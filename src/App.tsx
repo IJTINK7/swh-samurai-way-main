725daf0c99a9c3a106e88b55e531a2ff8fcda385
IJTINK7 <ijtink7@mail.ru>
2023-08-02 16:47:53 +0300

    Adding Home.tsx and Messages.tsx file to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import './App.css';
+import {Home} from "./components/Home";
+import {Messages} from "./components/Messages";
+
+export type DataOneType = {
+	image: string
+	button: string
+}
+
+export type DataTwoObjType = {
+	id: number
+	name: string
+	message: string
+}
 
 function App() {
-  return (
-    <div className="App">
-     Samurai will help!!!
-    </div>
-  );
+	const data1 = {
+		image: "The image should be here",
+		button: "The name of the button"
+	}
+
+	const data2: DataTwoObjType[]  = [
+		{id: 1, name: 'Alex', message: "Hi."},
+		{id: 2, name: 'Anna', message: "Hello."},
+		{id: 3, name: 'Roma', message: "I'm sorry, I'm busy!"},
+		{id: 4, name: 'Irina', message: "How r u?"},
+		{id: 5, name: 'Dmitry', message: "Well done!"},
+		{id: 6, name: 'Kate', message: "Well, it was my bad."},
+	]
+
+	return (
+		<div className="App">
+			Samurai will help!!!
+			<Home data1={data1}/>
+			<Messages data2={data2}/>
+		</div>
+	);
 }
 
 export default App;
